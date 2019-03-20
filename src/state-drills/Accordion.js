56565c08cd829c2b2f0188878924317b2249854f
David Bolin <david@bolin.at>
2019-03-20 11:04:47 -0700

    accordion 1 sections prop

--- src/state-drills/Accordion.js
+++ src/state-drills/Accordion.js
@@ -1,6 +1,11 @@
 import React from 'react';
 
 export default class Accordion extends React.Component {
+  
+  static defaultProps = {
+    sections: []
+  }
+  
   render() {
     return (
     <div>
