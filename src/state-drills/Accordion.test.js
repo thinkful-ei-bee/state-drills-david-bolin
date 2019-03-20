2d8ea44654a4406001995b250a253f869ecff0d0
David Bolin <david@bolin.at>
2019-03-20 11:47:25 -0700

    completed snapshot testing

--- src/state-drills/Accordion.test.js
+++ src/state-drills/Accordion.test.js
@@ -1,9 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Accordion from './Accordion';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Accordion />, div);
-  ReactDOM.unmountComponentAtNode(div);
+const sections = [
+  {
+    title: 'Section 1',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+  },
+  {
+    title: 'Section 2',
+    content: 'Cupiditate tenetur aliquam necessitatibus id distinctio quas nihil ipsam nisi modi!',
+  },
+  {
+    title: 'Section 3',
+    content: 'Animi amet cumque sint cupiditate officia ab voluptatibus libero optio et?',
+  },
+];
+
+describe('Accordion', () => {
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Accordion />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+  
+  it('renders empty li when sections is not supplied', () => {
+    const wrapper = shallow(<Accordion />);
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+
+  it('renders no sections active by default', () => {
+    const wrapper = shallow(<Accordion sections={sections} />);
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+
+  it('opens a clicked section', () => {
+    const wrapper = shallow(<Accordion sections={sections} />);
+    wrapper.find('button').at(1).simulate('click');
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+
+  it('only opens last section when multiple sections are clicked', () => {
+    const wrapper = shallow(<Accordion sections={sections} />);
+    wrapper.find('button').at(0).simulate('click');
+    wrapper.find('button').at(2).simulate('click');
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
 });
+
