cd992687ecb41c63d8a633e8c73bfd6f276dc1ac
David Bolin <david@bolin.at>
2019-03-20 08:56:33 -0700

    drill 3 steps 2-3

--- src/state-drills/RouletteGun.js
+++ src/state-drills/RouletteGun.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 class RouletteGun extends React.Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <p>you're safe!</p>
+      </div>
+    );
   }
 }
 
