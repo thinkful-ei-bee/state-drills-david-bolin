7e01edafc7fdd857e19dbed615ae27517e432866
David Bolin <david@bolin.at>
2019-03-20 08:37:55 -0700

    drill 2 req 2

--- src/state-drills/Bomb.js
+++ src/state-drills/Bomb.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
 class Bomb extends React.Component {
+  state = {
+    content: 'tick'
+  }
+  
   render() {
-    return <div />;
+    return (
+      <div>
+        <p>{this.state.content}</p>
+      </div>
+    );
   }
 }
 
