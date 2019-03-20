885accff1d6fff4e06acb980e1d178f75b13254a
David Bolin <david@bolin.at>
2019-03-20 09:15:36 -0700

    drill 3 step 7 complete

--- src/state-drills/RouletteGun.js
+++ src/state-drills/RouletteGun.js
@@ -15,8 +15,18 @@ class RouletteGun extends React.Component {
     this.setState({
       spinningTheChamber: true
     });
+    this.timeout = setTimeout(() => {
+      const randNum = Math.ceil(Math.random() * 8);
+      this.setState({
+        chamber: randNum,
+        spinningTheChamber: false 
+      });
+    }, 2000);
   }
 
+  componentWillUnmount() {
+    clearTimeout(this.timeout);
+  }
   render() {
     return (
       <div>
