f403db5379734b58afe61b475b3207551b0dca60
David Bolin <david@bolin.at>
2019-03-20 11:20:35 -0700

    functionality finished but not tests yet

--- src/state-drills/Accordion.js
+++ src/state-drills/Accordion.js
@@ -2,14 +2,25 @@ import React from 'react';
 
 export default class Accordion extends React.Component {
   
+  state = {
+    openSection: null
+  }
+
   static defaultProps = {
     sections: []
   }
   
+  handleButtonClicked(btn) {
+    this.setState({
+      openSection: btn
+    });
+  }
+
   render() {
     const liArray = this.props.sections.map((section, i) => 
       <li key={i}>
-        <button>{section.title}</button>
+        <button onClick={() => this.handleButtonClicked(i)}>{section.title}</button>
+        {this.state.openSection === i && <p>{section.content}</p>}
       </li>
     );
 
