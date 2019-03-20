3afec8d3eaaedb0fc17a23043718238052344fcb
David Bolin <david@bolin.at>
2019-03-20 10:57:52 -0700

    started Accordion project

---
+++ src/state-drills/Accordion.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Accordion extends React.Component {
+  render() {
+    return (
+    <div>
+
+    </div>
+    );
+  }
+}
---
+++ src/state-drills/Accordion.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Accordion from './Accordion';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Accordion />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
